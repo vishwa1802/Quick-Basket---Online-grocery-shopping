0210852f01f572e36f19e644d096a38d9925aaa8
Vishal <vishalvishwakarma2486@gmail.com>
2026-03-31 00:24:01 +0530

    Added Quick Basket project with README

---
+++ online grocery shop project/Online Grocery Shopping/services.php
@@ -0,0 +1,174 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Grocery Store</title>
+<body>
+<?php
+require 'dbcon.php';
+require 'header.php';
+?>
+		<div class="w3l_banner_nav_right">
+<!-- services -->
+		<div class="services">
+			<h3>Services</h3>
+			<div class="w3ls_service_grids">
+				<div class="col-md-5 w3ls_service_grid_left">
+					<h4>Home Delivery Service</h4>
+					<p>we can provide home delivery service near area within 10km to all user.
+					</p>
+				</div>
+				<div class="col-md-7 w3ls_service_grid_right">
+					<div class="col-md-4 w3ls_service_grid_right_1">
+						<img src="images/18.jpg" alt=" " class="img-responsive" />
+					</div>
+					<div class="col-md-4 w3ls_service_grid_right_1">
+						<img src="images/19.jpg" alt=" " class="img-responsive" />
+					</div>
+					<div class="col-md-4 w3ls_service_grid_right_1">
+						<img src="images/20.jpg" alt=" " class="img-responsive" />
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="w3ls_service_grids1">
+				<div class="col-md-6 w3ls_service_grids1_left">
+					<img src="images/4.jpg" alt=" " class="img-responsive" />
+				</div>
+				<div class="col-md-6 w3ls_service_grids1_right">
+					<ul>
+						<li><i class="fa fa-long-arrow-right" aria-hidden="true"></i>et voluptates repudiandae sint et molestiae</li>
+						<li><i class="fa fa-long-arrow-right" aria-hidden="true"></i>rerum necessitatibus saepe eveniet ut</li>
+						<li><i class="fa fa-long-arrow-right" aria-hidden="true"></i>placeat facere possimus, omnis voluptas</li>
+						<li><i class="fa fa-long-arrow-right" aria-hidden="true"></i>Et harum quidem rerum facilis est et expedita</li>
+						<li><i class="fa fa-long-arrow-right" aria-hidden="true"></i>similique sunt in culpa qui officia deserunt</li>
+						<li><i class="fa fa-long-arrow-right" aria-hidden="true"></i>odio dignissimos ducimus qui blanditiis</li>
+					</ul>
+					<a href="single.html">Shop Now</a>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+<!-- //services -->
+		</div>
+		<div class="clearfix"></div>
+	</div>
+<!-- //banner -->
+<!-- services-bottom -->
+	<div class="services-bottom">
+		<div class="container">
+			<div class="col-md-3 about_counter_left">
+				<i class="glyphicon glyphicon-user" aria-hidden="true"></i>
+				<p class="counter">89,147</p> 
+				<h3>Followers</h3>
+			</div>
+			<div class="col-md-3 about_counter_left">
+				<i class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></i>
+				<p class="counter">54,598</p> 
+				<h3>Savings</h3>
+			</div>
+			<div class="col-md-3 about_counter_left">
+				<i class="glyphicon glyphicon-export" aria-hidden="true"></i>
+				<p class="counter">83,983</p> 
+				<h3>Support</h3>
+			</div>
+			<div class="col-md-3 about_counter_left">
+				<i class="glyphicon glyphicon-bullhorn" aria-hidden="true"></i>
+				<p class="counter">45,894</p> 
+				<h3>Popularity</h3>
+			</div>
+			<div class="clearfix"> </div>
+			<!-- Stats-Number-Scroller-Animation-JavaScript -->
+				<script src="js/waypoints.min.js"></script> 
+				<script src="js/counterup.min.js"></script> 
+				<script>
+					jQuery(document).ready(function( $ ) {
+						$('.counter').counterUp({
+							delay: 10,
+							time: 1000
+						});
+					});
+				</script>
+			<!-- //Stats-Number-Scroller-Animation-JavaScript -->
+
+		</div>
+	</div>
+<!-- //services-bottom -->
+
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //newsletter -->
+<!-- footer -->
+
+			</div>
+			<div class="wthree_footer_copy">
+				<p> Grocery Store. All rights reserved | Design by <a href="http://flipkart.com/">Project</a></p>
+			</div>
+		</div>
+	</div>
+<!-- //footer -->
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+<!-- //here ends scrolling icon -->
+<script src="js/minicart.js"></script>
+<script>
+		paypal.minicart.render();
+
+		paypal.minicart.cart.on('checkout', function (evt) {
+			var items = this.items(),
+				len = items.length,
+				total = 0,
+				i;
+
+			// Count the number of each item in the cart
+			for (i = 0; i < len; i++) {
+				total += items[i].get('quantity');
+			}
+
+			if (total < 3) {
+				alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
+				evt.preventDefault();
+			}
+		});
+
+	</script>
+</body>
+</html>
